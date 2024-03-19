151f89b0f8e6115df40031b5e06f274ade1a0026
shaikathaque <shaikathaque4@gmail.com>
2024-03-19 22:19:02 +0600

    setup and run prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': ['error']
   },
 }
