96e0e7d8ba0f6df093a2de26eb9729d3402c5c3b
shaikathaque <shaikathaque4@gmail.com>
2024-03-20 03:08:13 +0600

    update: readme

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="flex min-h-screen flex-col">
       <SiteHeader />
       <div className="flex flex-1 items-center justify-center">
-        <h1>Responsive Navbar Example using Shadcn/ui components</h1>
+        <h1>Responsive Navbar using Shadcn/ui</h1>
       </div>
     </div>
   );
